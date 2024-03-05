2130e3f6e6dddc739b154fde41047547b777165a
Sneh Mehta <smehta@colum.edu>
2024-03-05 10:03:11 -0600

    chore: removed esbuild

--- build.js
+++
@@ -1,9 +0,0 @@
-import { build } from "esbuild";
-
-build({
-  entryPoints: ["app.js"],
-  bundle: true,
-  outfile: "dist/bundle.js",
-  minify: true,
-  sourcemap: true,
-}).catch(() => process.exit(1));
