8b15d5bb59fc886719a972fee870597555641f06
Sneh Mehta <smehta@colum.edu>
2024-08-14 16:40:55 -0500

    feat: home page done - no search logic implemented

---
+++ components/layout/appFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="bg-orange-400 text-cream-100 py-4">
+        <div class="container mx-auto px-4 text-center">
+            <span>&copy; Hampshire Pharmacy | </span>
+            <NuxtLink to="/privacy" class="hover:underline">Privacy Policy</NuxtLink>
+            <span> | </span>
+            <NuxtLink to="/faq" class="hover:underline">FAQs</NuxtLink>
+            <span> | </span>
+            <NuxtLink to="/about" class="hover:underline">About Us</NuxtLink>
+        </div>
+    </footer>
+</template>
---
+++ components/searchBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-center mb-12 bg-cream-200 py-8 mx-auto">
+        <h1 class="text-4xl font-bold mb-4">Affordable Medications, Trusted Service</h1>
+        <p class="text-xl mb-8">Access reliable generic drugs at fair prices, no insurance needed</p>
+        <div class="flex justify-center">
+            <input v-model="searchValue" type="text" placeholder="Search the entire pharmacy here..."
+                class="w-1/2 px-4 py-2 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-pharmaBlue-400" />
+            <button @click="handleSearch"
+                class="bg-orange-400 text-white font-semibold px-6 py-2 rounded-r-lg hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-400 transition duration-150 ease-in-out">
+                Save on prescriptions
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const searchValue = ref('')
+
+const handleSearch = () => {
+    // Handle search logic here
+    console.log('Searching for:', searchValue.value)
+}
+</script>
