b7cdebb8ad534f4a6cecb509bbd2b5a8457731fe
Sneh Mehta <smehta@colum.edu>
2024-08-20 11:28:25 -0500

    feat: added SEO information in header in nuxtConfig, and added site manifest, robots.txt, sitemap files for later use once deployed to domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,45 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  app: {
+    head: {
+        title: "Hampshire Pharmacy | Low Cost Generic Alternatives",
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/public/favicon.ico'},
+            {rel: 'canonical', href: 'https://hampshirepharmacy.com'}
+        ],
+        meta: [
+            {charset: "utf-8"},
+            {name: "viewport", content: "width=device-width, initial-scale=1.0"},
+            {
+                name: "description",
+                content: "Reliable low cost generic alternatives for prescription drugs from a local and trustworthy pharmacy"
+            },
+            {
+                name: "keywords",
+                content: "pharmacy near me, cash pharmacy, online pharmacy, generic alternatives, generic drugs, drugstore, prescriptions, generics, pharmacy, discounted medication, shop online, healthcare"
+            },
+            // Open Graph / Facebook
+            {property: "og:type", content: "website"},
+            {property: "og:url", content: "https://hampshirepharmacy.com"},
+            {property: "og:title", content: "Hampshire Pharmacy | Low Cost Generic Alternatives"},
+            {
+                property: "og:description",
+                content: "Reliable low cost generic alternatives for prescription drugs from a local and trustworthy pharmacy"
+            },
+            {property: "og:image", content: "/public/images/HampshireRXMetaImage.jpg"},
+            // Twitter
+            {name: "twitter:card", content: "summary_large_image"},
+            {name: "twitter:url", content: "https://hampshirepharmacy.com"},
+            {name: "twitter:title", content: "Hampshire Pharmacy | Low Cost Generic Alternatives"},
+            {
+                name: "twitter:description",
+                content: "Reliable low cost generic alternatives for prescription drugs from a local and trustworthy pharmacy"
+            },
+            {name: "twitter:image", content: "/public/images/HampshireRXMetaImage.jpg"},
+        ],
+    },
+},
   modules: ["@nuxtjs/tailwindcss", 'nuxt-mongoose', 'nuxt-graphql-server','@nuxtjs/apollo'],
   graphqlServer: {
     url: '/api/graphql',
