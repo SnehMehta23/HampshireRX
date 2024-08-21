0738cb70cf7ff842e5f07eca31ca9cadf9f8f9be
Sneh Mehta <smehta@colum.edu>
2024-08-21 00:31:36 -0500

    feat: removed how to in three steps text from hero section on tertiary pages

--- pages/faq.vue
+++ pages/faq.vue
@@ -17,12 +17,6 @@
                     <span class="underline-animation">no insurance needed!</span>
                 </p>
             </div>
-            <div class="flex flex-col items-center justify-center mb-4 2xl:mb-6 text-dark-charcoal">
-                <span
-                    class="animate-fade-in-left text-md 2xl:text-5xl dark:text-white bg-white/20 p-2 border border-white/40 shadow-lg backdrop-blur-lg rounded-full xl:mb-0 mb-2">How
-                    Hampshire Pharmacy Works <span class="text-orange-500">In Just Three
-                        Steps</span></span>
-            </div>
 
             <HowToCards />
             <!-- SearchBar positioned absolutely -->
