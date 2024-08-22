48391053e3f086acee1e20543c0c5a9b2c107ea6
Sneh Mehta <smehta@colum.edu>
2024-08-22 17:14:35 -0500

    feat: one last hotfix for this searchBar on mobile

--- components/searchBar.vue
+++ components/searchBar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full">
-        <div class="flex overflow-hidden rounded-full border border-gray-300 shadow-inner">
+        <div class="flex overflow-hidden rounded-tl-full rounded-bl-full border border-gray-300 shadow-inner">
             <input v-model="searchValue" type="text" placeholder="Browse pharmacy catalog..."
                 class="w-3/4 px-6 py-4 border border-gray-300 rounded-l-lg xl:rounded-l-full focus:outline-none focus:ring-2 focus:ring-pharmaBlue-400 shadow-lg" />
             <button @click="handleSearch" class="w-1/2 bg-orange-400 text-white font-semibold py-4 rounded-r-full xl:rounded-r-full
