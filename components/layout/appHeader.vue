2eb9faf4d6b43d3ef4847baaca82100e2972875e
Sneh Mehta <smehta@colum.edu>
2024-08-26 12:11:34 -0500

    feat: added some fixes for the hamburger menu and other pages - moved logo to appHeader

--- components/layout/appHeader.vue
+++ components/layout/appHeader.vue
@@ -1,61 +1,57 @@
 <template>
     <header class="bg-pharmaBlue-400 text-secondary py-4">
-        <div class="container mx-auto px-4 flex items-center text-white text-base">
-            <div class="hidden sm:flex ml-2">
-                <img class="w-6 h-6" src="/public/images/svg/prescription.svg" alt="">
-                <p class="text-white ml-2">Hours: Mon-Fri 10am-6pm, Sat 10am-2pm</p>
+        <div class="mx-auto px-4">
+            <div class="flex items-center justify-between">
+                <!-- Logo -->
+                <div class="">
+                    <NuxtLink to="/" class="">
+                        <img src="~/public/images/logos/secondaryWideLogo.png" alt="Hampshire Pharmacy Logo"
+                            class="w-64" />
+                    </NuxtLink>
+                </div>
+
+                <!-- Navigation -->
+                <nav class="hidden sm:flex items-center space-x-6 sm:hidden xl:inline-flex">
+                    <NuxtLink v-if="currentRoute !== '/'" to="/" @click="handleNavClick('Home', '/')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200 relative">
+                        Home
+                    </NuxtLink>
+                    <NuxtLink v-if="currentRoute !== '/about'" to="/about" @click="handleNavClick('About', '/about')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200 relative">
+                        About
+                    </NuxtLink>
+                    <NuxtLink v-if="currentRoute !== '/privacy'" to="/privacy"
+                        @click="handleNavClick('Privacy', '/privacy')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200 relative">
+                        Privacy
+                    </NuxtLink>
+                    <NuxtLink v-if="currentRoute !== '/faq'" to="/faq" @click="handleNavClick('FAQ', '/faq')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200 relative">
+                        FAQ
+                    </NuxtLink>
+                    <NuxtLink v-if="currentRoute !== '/transfer-prescription'" to="/transfer-prescription"
+                        @click="handleNavClick('Prescription Transfer', '/transfer-prescription')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200 relative">
+                        Prescription Transfer
+                    </NuxtLink>
+                    <!-- Facebook Icon Link -->
+                    <NuxtLink to="https://www.facebook.com/HampshirePharmacy/" target="_blank" rel="noopener noreferrer"
+                        @click="handleNavClick('Facebook', 'https://www.facebook.com/HampshirePharmacy/')"
+                        class="text-white hover:text-orange-500 transition-colors duration-200">
+                        <img class="hover:opacity-75" src="/public/images/logos/Facebook_Logo_Primary.png"
+                            alt="Facebook" width="24" height="24">
+                    </NuxtLink>
+                </nav>
+
+                <!-- Hamburger Menu Icon for Mobile -->
+                <button @click="toggleMobileMenu" class="sm:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16m-7 6h7" />
+                    </svg>
+                </button>
             </div>
-
-            <!-- Desktop Navigation -->
-            <nav class="ml-auto hidden sm:flex space-x-6">
-                <NuxtLink v-if="currentRoute !== '/'" to="/" @click="handleNavClick('Home', '/')"
-                    class="hover:underline hover:text-orange-500 transition-colors duration-200 relative">
-                    Home
-                    <span
-                        class="absolute left-0 bottom-0 h-0.5 bg-orange-500 w-full scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </NuxtLink>
-                <NuxtLink v-if="currentRoute !== '/about'" to="/about" @click="handleNavClick('About', '/about')"
-                    class="hover:underline hover:text-orange-500 transition-colors duration-200 relative">
-                    About
-                    <span
-                        class="absolute left-0 bottom-0 h-0.5 bg-orange-500 w-full scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </NuxtLink>
-                <NuxtLink v-if="currentRoute !== '/privacy'" to="/privacy"
-                    @click="handleNavClick('Privacy', '/privacy')"
-                    class="hover:underline hover:text-orange-500 transition-colors duration-200 relative">
-                    Privacy
-                    <span
-                        class="absolute left-0 bottom-0 h-0.5 bg-orange-500 w-full scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </NuxtLink>
-                <NuxtLink v-if="currentRoute !== '/faq'" to="/faq" @click="handleNavClick('FAQ', '/faq')"
-                    class="hover:underline hover:text-orange-500 transition-colors duration-200 relative">
-                    FAQ
-                    <span
-                        class="absolute left-0 bottom-0 h-0.5 bg-orange-500 w-full scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </NuxtLink>
-                <NuxtLink v-if="currentRoute !== '/prescription-transfer'" to="/prescription-transfer"
-                    @click="handleNavClick('About', '/about')"
-                    class="hover:underline hover:text-orange-500 transition-colors duration-200 relative">
-                    Prescription Transfer
-                    <span
-                        class="absolute left-0 bottom-0 h-0.5 bg-orange-500 w-full scale-x-0 hover:scale-x-100 transition-transform duration-300 origin-left"></span>
-                </NuxtLink>
-                <!-- Facebook Icon Link -->
-                <NuxtLink to="https://www.facebook.com/HampshirePharmacy/" target="_blank" rel="noopener noreferrer"
-                    @click="handleNavClick('Facebook', 'https://www.facebook.com/HampshirePharmacy/')"
-                    class="hover:text-orange-500 transition-colors duration-200">
-                    <img class="hover:opacity-75" src="/public/images/logos/Facebook_Logo_Primary.png" alt="Facebook"
-                        width="24" height="24">
-                </NuxtLink>
-            </nav>
-
-            <!-- Hamburger Menu Icon for Mobile -->
-            <button @click="toggleMobileMenu" class="ml-auto sm:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
-            </button>
         </div>
 
         <!-- Mobile Menu Overlay -->
@@ -85,7 +81,8 @@
                         class="text-white hover:text-orange-500 transition-colors duration-200">
                         FAQ
                     </NuxtLink>
-                    <NuxtLink to="/about" @click="handleNavClick('Prescription Transfer', '/prescription-transfer')"
+                    <NuxtLink to="/transfer-prescription"
+                        @click="handleNavClick('Prescription Transfer', '/transfer-prescription')"
                         class="text-white hover:text-orange-500 transition-colors duration-200">
                         Prescription Transfer
                     </NuxtLink>
