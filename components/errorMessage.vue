2f5c576615eceabf034ad2b98bde489c16d60961
sunny <raphy@raphsant.com>
2024-08-26 11:45:45 -0400

    Add errorMessage component
    Introduce errorMessage.vue component to display error messages with customizable text. The component uses TypeScript and scoped styling, ensuring consistency and reusability across the application.

---
+++ components/errorMessage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const {text} = defineProps(['text'])
+</script>
+
+<template>
+
+  <div class="text-md md:text-lg text-red-600 font-light">
+    {{ text }}
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
