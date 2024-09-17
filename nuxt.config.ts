573efce5be1f598020613ad2cd106d7e8934db6d
Rafael Santana <55399350+Raphsant@users.noreply.github.com>
2024-09-17 16:12:08 -0500

    Update nuxt.config.ts Sneh fault
    Sneh fault

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,8 +118,8 @@ export default defineNuxtConfig({
     apollo: {
         clients: {
             default: {
-                httpEndpoint: 'http://localhost:3001/api/graphql',
-                wsEndpoint: 'ws://localhost:3001/api/graphql', // WebSocket endpoint for subscriptions
+                httpEndpoint: 'http://hampshirepharmacy/api/graphql',
+                wsEndpoint: 'ws://hampshirepharmacy/api/graphql/api/graphql', // WebSocket endpoint for subscriptions
                 httpLinkOptions: {
                     credentials: 'include'
                 }
