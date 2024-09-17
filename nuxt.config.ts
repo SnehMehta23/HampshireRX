41e3c82a86dae7b8665734a59c5394279a2c1198
Rafael Santana <55399350+Raphsant@users.noreply.github.com>
2024-09-17 16:18:00 -0500

    Update nuxt.config.ts sneh fault

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,8 +118,8 @@ export default defineNuxtConfig({
     apollo: {
         clients: {
             default: {
-                httpEndpoint: 'http://hampshirepharmacy/api/graphql',
-                wsEndpoint: 'ws://hampshirepharmacy/api/graphql/api/graphql', // WebSocket endpoint for subscriptions
+                httpEndpoint: 'http://hampshirepharmacy.com',
+                wsEndpoint: 'ws://hampshirepharmacy.com', // WebSocket endpoint for subscriptions
                 httpLinkOptions: {
                     credentials: 'include'
                 }
